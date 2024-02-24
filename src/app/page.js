be54c31893db9f9afa316f32586bf0f9117d48d6
Tanesya <tanesya.t@gmail.com>
2024-02-24 21:50:06 +0700

    fix: issue with getData not refreshed after router.refresh

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,17 @@ import { CreateItem } from "@/components/CreateItem";
 import { getData } from "@/lib/data";
 import { ItemTable } from "@/components/ItemTable";
 
-const { data } = await getData();
-const groups = [...new Set(data.map((x) => x.group))];
-
 export default async function Home() {
+  const { data } = await getData();
+  const groups = [...new Set(data.map((x) => x.group))];
+
   return (
     <main>
       <div className="max-w-screen-md mx-auto mt-5">
         <div className="flex items-center justify-between gap-1 mb-5">
           <CreateItem />
         </div>
-        <ItemTable data={data} groups={groups} />
+          <ItemTable data={data} groups={groups} />
       </div>
     </main>
   );
