de26ac7e2d3dbc558238f19a0e8bb5ed3143aba6
Tanesya <tanesya.t@gmail.com>
2024-02-26 01:10:26 +0700

    feat: add group total in table

--- src/components/ItemGroup.jsx
+++ src/components/ItemGroup.jsx
@@ -1,3 +1,4 @@
+import { calculateSum, getTotalValues } from "@/lib/numbers";
 import { Items } from "@/components/Items";
 
 export const ItemGroup = ({ data, groups }) => {
@@ -5,10 +6,15 @@ export const ItemGroup = ({ data, groups }) => {
     <>
       {groups.map((item) => {
         const filtered = data.filter((i) => i.group === item);
+        const { budget, spend, remaining } = getTotalValues(filtered);
+
         return (
           <>
             <tr className="table-group">
-              <td className="table-group__data" colSpan="5">{item}</td>
+              <td className="table-group__data">{item}</td>
+              <td className="table-group__data">{budget}</td>
+              <td className="table-group__data">{spend}</td>
+              <td className="table-group__data">{remaining}</td>
             </tr>
             <Items data={filtered} />
           </>
--- src/lib/numbers.js
+++ src/lib/numbers.js
@@ -7,12 +7,32 @@ const rupiah = (number) => {
 
 export const formatItemNum = (item) => {
   const budget = item.budget_plan || 0;
-  const spend = item.budget_actual|| 0;
+  const spend = item.budget_actual || 0;
   const remaining = budget - spend;
 
   return {
     budget: rupiah(budget),
     spend: rupiah(spend),
-    remaining: rupiah(remaining)
+    remaining: rupiah(remaining),
+  };
+};
+
+const calculateSum = (data, prop) => {
+  const total = data.reduce((accumulator, object) => {
+    return accumulator + Number(object[prop]) || 0;
+  }, 0);
+
+  return total;
+};
+
+export const getTotalValues = (data) => {
+  const budget = calculateSum(data, "budget_plan");
+  const spend = calculateSum(data, "budget_actual");
+  const remaining = budget - spend;
+
+  return {
+    budget: rupiah(budget),
+    spend: rupiah(spend),
+    remaining: rupiah(remaining),
   };
 };
